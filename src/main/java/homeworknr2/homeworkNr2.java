e1935b86e7bd06a735be7e569120379a5d69e6eb
User <dima724053@gmail.com>
2023-09-13 17:57:21 +0300

    homework 2 commit

--- src/main/java/homeworknr2/homeworkNr2.java
+++ src/main/java/homeworknr2/homeworkNr2.java
@@ -2,6 +2,27 @@ package homeworknr2;
 
 public class homeworkNr2 {
     public static void main(String[] args) {
+        System.out.println("Helo world");
 
-    }
+        long CurrentWorldPopulation = 7811080815L;
+        int CarsProducedThisYear = 55235358;
+        char FirstLetterInAlphabet = 'A';
+        boolean JavaIsAmazing  = true;
+        double Pi =3.14159;
+
+        System.out.println( "CurrentWorldPopulation"+ CurrentWorldPopulation );
+        System.out.println( "FirstLetterInAlphabet "+ FirstLetterInAlphabet  );
+        System.out.println( "JavaIsAmazing "+ JavaIsAmazing  );
+        System.out.println( "Pi"+Pi);
+double Price = 99.9;
+double Tax = 20.5;
+int Quantity = 10;
+double Sum = 0;
+
+   double total =Price+ Tax *  Quantity+ Sum;
+        System.out.println("Price:"+Price);
+        System.out.println( "Tax:"+Tax);
+        System.out.println( "Quantity:"+Quantity);
+        System.out.println( "Sum:"+Sum);
+}
 }
