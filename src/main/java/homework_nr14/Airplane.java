3b50c6a7101b243ad12875568012532cb5209a8e
User <dima724053@gmail.com>
2023-11-06 17:06:44 +0200

    homework 14 commit

---
+++ src/main/java/homework_nr14/Airplane.java
@@ -0,0 +1,11 @@
+package homework_nr14;
+
+import java.io.Serializable;
+
+public class Airplane extends Vehicle implements Serializable {
+    public Airplane(String name) {
+        super(name);
+    }
+
+
+}
